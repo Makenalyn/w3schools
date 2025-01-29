2f933da4eacf1c457e9670533487db216057e98e
makenalyn <makenalynton@gmail.com>
2025-01-29 21:05:55 +0300

    prints the document of a given module

--- firsthundred.py
+++ firsthundred.py
@@ -8,6 +8,17 @@ def n_nn_nnn(num):
 
     print("{}".format(n1 + n2 + n3))
 
+
+#print documents of python builtin functions
+def printdocuments(module_name):
+    try:
+        module = __import__(module_name)
+        return module.__doc__
+    except ImportError:
+        return f"Module {module_name} not found"
+
 if __name__ == "__main__":
     mynum = 5
     n_nn_nnn(mynum)
+    string = "math"
+    print(printdocuments(string))
